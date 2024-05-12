ae3de570dbe0ec22f2c0b9bf87d3734dfc0335d5
ilonautkevich <149751920+ilonautkevich@users.noreply.github.com>
2024-05-12 11:56:39 +0500

    Update test.py

--- src/tests/test.py
+++ src/tests/test.py
@@ -4,7 +4,7 @@ from unittest.mock import patch, MagicMock
 from main import load_model, preprocess_image, print_predictions  # Импортируйте функции из вашего файла
 
 class TestApp(unittest.TestCase):
-
+    
     @patch('main.EfficientNetB0')
     def test_load_model(self, mock_EfficientNetB0):
         mock_model = MagicMock()
@@ -12,7 +12,7 @@ class TestApp(unittest.TestCase):
         model = load_model()
         self.assertEqual(model, mock_model)
         mock_EfficientNetB0.assert_called_once_with(weights='imagenet')
-
+        
     def test_preprocess_image(self):
         img = Image.new('RGB', (224, 224), color=(73, 109, 137))
         x = preprocess_image(img)
