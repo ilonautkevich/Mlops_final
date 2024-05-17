8716c6937c53412a924ac5540b74abcd7867e293
Natalia-mswm <149807177+Natalia-mswm@users.noreply.github.com>
2024-05-17 12:42:35 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,8 +5,6 @@ from main import load_model, preprocess_image, print_predictions
 
 
 class TestImageClassificationApp(unittest.TestCase):
-
-    
     def setUp(self):
         self.model = load_model()
 
@@ -14,13 +12,13 @@ class TestImageClassificationApp(unittest.TestCase):
     def test_load_model(self):
         self.assertIsNotNone(self.model, "Модель не загружена")
 
-    
+
     def test_preprocess_image(self):
         img = Image.new('RGB', (224, 224))
         preprocessed_img = preprocess_image(img)
         self.assertIsInstance(preprocessed_img, np.ndarray, "Изображение не было правильно предварительно обработано")
 
-    
+
     def test_print_predictions(self):
     # Создаем тестовые предсказания с формой (1, 1000)
         preds = np.random.rand(1, 1000)
