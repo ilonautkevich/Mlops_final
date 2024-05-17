4815a4455fb18f359618f7765465b13b25dee2d3
Natalia-mswm <149807177+Natalia-mswm@users.noreply.github.com>
2024-05-17 12:47:25 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,22 +5,22 @@ from main import load_model, preprocess_image, print_predictions
 
 
 class TestImageClassificationApp(unittest.TestCase):
-    def setUp(self):
+  def setUp(self):
         self.model = load_model()
 
-    
-    def test_load_model(self):
+
+  def test_load_model(self):
         self.assertIsNotNone(self.model, "Модель не загружена")
 
 
-    def test_preprocess_image(self):
+  def test_preprocess_image(self):
         img = Image.new('RGB', (224, 224))
         preprocessed_img = preprocess_image(img)
         self.assertIsInstance(preprocessed_img, np.ndarray, "Изображение не было правильно предварительно обработано")
 
 
-    def test_print_predictions(self):
-    # Создаем тестовые предсказания с формой (1, 1000)
+  def test_print_predictions(self):
+  # Создаем тестовые предсказания с формой (1, 1000)
         preds = np.random.rand(1, 1000)
         print_predictions(preds)
         # Здесь мы предполагаем, что функция print_predictions выводит результаты в stdout,
